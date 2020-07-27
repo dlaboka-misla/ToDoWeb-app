b2c3f61eb1641e96554be8aa8e31b115426d4371
dlaboka-misla <dimitrievaalexandra@seznam.cz>
2020-07-27 14:55:23 +0200

    refactor code

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # to-do web app
 Responsive Web Application for one user. The user can make to-do List for the current week, Sunday till Saturday.
+
 The page reloads each week, i.e. the tasks remain and are visible only for the current week.
+
+The dates follow the current time.
+
 Cannot edit the task, only check/cross if completed.
+
 To navigate through the days, click on the curly brackets or left-right arrow keys.
+
 To enter text, click on the arrow or press enter.
 
-Written in JavaScript using node.js on server side together with several dependencies like express, body-parser, dateformat and ejs template.
+Written in JavaScript using **Node.js** on server side and **MongoDB** as database, together with several dependencies like express, body-parser, dateformat, ejs template and mongoose.
 
 To test and use, assuming you've already installed docker, run these commands in the console:
 
--- app.js
+++ app.js
@@ -25,45 +25,13 @@ const Item = mongoose.model("Item", itemsSchema);
 let day = date.getDate();
 let dayOfWeek = new Date().getDay();
 let currentDayOfWeek = new Date().getDay();
-let itemsMap = new Map();
-let itemsCheckedMap = new Map();
+let itemsMap = [];
 let lastAccessedDay = -1;
 let lastAccessedDate;
 
-// reloadItems is called when the app starts and when the week changes
-function reloadItems() {
-    day = date.getDate();
-    resetItems();
-};
-
-/* each day of the week has its own array of items.
- itemsMap stores unchecked items, itemsCheckedMap stores already done (checked) items.
-*/
-
-function resetItems() {
-    itemsMap = new Map();
-    itemsMap.set(0, []);
-    itemsMap.set(1, []);
-    itemsMap.set(2, []);
-    itemsMap.set(3, []);
-    itemsMap.set(4, []);
-    itemsMap.set(5, []);
-    itemsMap.set(6, []);
-    itemsCheckedMap = new Map();
-    itemsCheckedMap.set(0, []);
-    itemsCheckedMap.set(1, []);
-    itemsCheckedMap.set(2, []);
-    itemsCheckedMap.set(3, []);
-    itemsCheckedMap.set(4, []);
-    itemsCheckedMap.set(5, []);
-    itemsCheckedMap.set(6, []);
-}
-
+// deletes items which were created in the past week, ending last Saturday
 function deleteFromDB() {
-   // db.gpsdatas.find({"createdAt" : { $gte : new ISODate("2012-01-12T20:15:31Z") }});
-   // { $gte: new Date(dateVar).toISOString() }
     const lastSunday = date.getDateOfLastSunday(new Date());
-    console.log("DateOfLastSunday = " + lastSunday);
     Item.deleteMany({"createdOn": { $lt : new Date(lastSunday.toISOString().slice(0,10)) } }, function(err) {
          if (err) return handleError(err);
     })
@@ -77,91 +45,55 @@ app.get("/", function(req, res) {
     if (lastAccessedDay === -1 ||
         (lastAccessedDay > currentDayOfWeek) ||
         (date.getDateDiff(currentDate, lastAccessedDate) >= 7)) {
-        reloadItems();
+        day = date.getDate();
         deleteFromDB();
     }
     lastAccessedDay = currentDayOfWeek;
     lastAccessedDate = currentDate;
-    resetItems();
-    Item.find({}, function(err, foundItems) {
+    itemsMap = [];
+    Item.find({day:dayOfWeek}, function(err, foundItems) {
         foundItems.forEach(foundItem => {
-            if (foundItem.day === dayOfWeek) {
-            let itemOption = {
-            itm: foundItem.name,
-            id: foundItem._id,
-            day: foundItem.day,
-            chk: foundItem.chk
-        };
-        itemsMap.get(dayOfWeek).push(itemOption);
-        }
+        itemsMap.push(foundItem);
     })
         res.render("list", {
         listTitle: day,
         dayOfWeek: dayOfWeek,
         newListItems: itemsMap,
-        itemsCheckedMap: itemsCheckedMap,
     });
     });
 });
 
-/* add new item (itemOption) to itemsMap. itemOption is a class which
-contains item send from the user and is a text input.
-chk:0 means item is unchecked */
-
+// adds new item to itemsMap
 app.post("/", function(req, res) {
     let item = req.body.newItem;
-    let itemOption = {
-        itm: item,
-        day: dayOfWeek,
-        chk: 0
-    };
     if (item.length !== 0) {
         if (item.length <= 300) {
-            itemsMap.get(dayOfWeek).push(itemOption);
             const todoItem = new Item ({
-                name: itemOption.itm,
+                name: item,
                 createdOn: new Date(new Date().toISOString().slice(0,10)),
-                day: itemOption.day,
-                chk: itemOption.chk
+                day: dayOfWeek,
+                chk: 0
             });
             todoItem.save();
+            itemsMap.push(todoItem);
         }
     }
     res.redirect("/");
 });
 
-// removes an item from itemsCheckedMap
-app.post("/onMyClick", function(req, res) {
+// marks to-do item as checked or unchecked in the database
+app.post("/onToggle", function(req, res) {
     let itemNumber = req.body.itemNumber;
-    const index = itemsCheckedMap.get(dayOfWeek).indexOf(itemNumber);
     let itemId = req.body.id;
-    console.log("itemId = " + itemId);
-    Item.updateOne({_id: itemId}, {$set: {chk: 0} }, function(err, res) {
+    let chk = req.body.chk;
+    Item.updateOne({_id: itemId}, {$set: {chk: chk} }, function(err, res) {
         if (err) throw err;
-         console.log("1 item unchecked");
     })
-    if (index > -1) {
-        itemsCheckedMap.get(dayOfWeek).splice(index, 1);
-    }
-    res.redirect("/");
-});
-
-// adds item to itemsCheckedMap
-app.post("/onYourClick", function(req, res) {
-    let itemNumber = req.body.itemNumber; 
-    let itemId = req.body.id;
-    console.log("itemId = " + itemId);
-    Item.updateOne({_id: itemId}, {$set: {chk: 1} }, function(err, res) {
-        if (err) throw err;
-         console.log("1 item checked");
-    })
-    itemsCheckedMap.get(dayOfWeek).push(itemNumber);
     res.redirect("/");
 });
 
 /* the function bellow is called when curly bracket is clicked or arrow key is pressed
   and changes the title of the week */
-
 app.post("/posts/:day/:type", function(req, res) {
     let currentDay = req.params.day;
     let type = req.params.type;
